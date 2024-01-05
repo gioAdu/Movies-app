711779915841c16b52d9da104f3b9fddf9acaa1b
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-05 17:33:43 +0400

    Add movie page server route

---
+++ src/routes/[...movies]/+page.server.js
@@ -0,0 +1,9 @@
+import { getMovies } from '../../lib/api/api.js';
+
+export async function load({ params }) {
+	const [type, id] = params.movies.split('/');
+
+	const movieDetails = await getMovies(`/${type}/${id}`);
+
+	return { movieDetails };
+}
