d51e7d29ff2c31c555847e3217ba8b9a77d1082e
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-08 16:51:11 +0400

    Remove unused movie page server code

--- src/routes/[...movies]/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { getMovies } from '$lib/api/api.js';
-
-export async function load({ params }) {
-	const [type, id] = params.movies.split('/');
-
-	const movieDetails = await getMovies(`/${type}/${id}`);
-
-	return { movieDetails };
-}
