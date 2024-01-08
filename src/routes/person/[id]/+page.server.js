87a8add9de270361d2c65218f1b93b37bf3d5a52
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-08 17:59:48 +0400

    Refactor variable name in +page.server.js

--- src/routes/person/[id]/+page.server.js
+++ src/routes/person/[id]/+page.server.js
@@ -3,9 +3,10 @@ import { getData } from '$lib/api/api.js';
 export async function load({ params }) {
   const id = params.id;
 
-  const person_info = await getData(`/person/${id}`);
+  const personInfo = await getData(`/person/${id}`);
   const credits_info = await getData(`/person/${id}/combined_credits`);
 
   const sortedList = credits_info.cast.sort((a, b) => b.popularity - a.popularity).slice(0, 15);
-  return { sortedList , person_info }
+  
+  return { sortedList , personInfo }
 }
