3ef846d2ac3ea757ead97c08935d568960d63675
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-08 16:51:01 +0400

    Add error page component

---
+++ src/routes/+error.svelte
@@ -0,0 +1,46 @@
+<section class="h-screen page_404">
+	<div class="container mx-auto px-4">
+		<div class="flex flex-wrap">
+			<div class="w-full">
+				<div class="w-4/5 mx-auto text-center">
+					<div class="my-8 four_zero_four_bg">
+						<h1 class="text-6xl">404</h1>
+					</div>
+
+					<div class="my-8 contant_box_404">
+						<h3 class="text-3xl">Look like you're lost</h3>
+
+						<p class="mt-4">the page you are looking for not avaible!</p>
+
+						<a href="/" class="mt-4 inline-block bg-error text-white font-bold py-2 px-4 rounded"
+							>Go to Home</a
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style>
+	.page_404 {
+		padding: 40px 0;
+		background: #fff;
+		font-family: 'Arvo', serif;
+	}
+
+	.four_zero_four_bg {
+		background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+		background-repeat: no-repeat;
+		height: 400px;
+		background-position: center;
+	}
+
+	.four_zero_four_bg h1 {
+		font-size: 80px;
+	}
+
+	.contant_box_404 {
+		margin-top: -50px;
+	}
+</style>
