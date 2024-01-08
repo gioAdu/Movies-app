f230933c6cda77ae739ccf8beffc963acbd9434f
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-08 16:51:05 +0400

    Update API function calls in +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,8 @@
-import { getMovies } from '../lib/api/api.js';
+import { getData } from '../lib/api/api.js';
 
-export async function load({  params }) {
-	const moviesData = await getMovies(`/trending/movie/day`);
-	const TVData = await getMovies(`/trending/tv/day`);
+export async function load({ params }) {
+	const moviesData = await getData(`/trending/movie/day`);
+	const TVData = await getData(`/trending/tv/day`);
 
-	return { moviesData , TVData };
+	return { moviesData, TVData };
 }
