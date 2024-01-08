2af2a910996497a8699a121c2fca5060971ad7dd
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-08 16:51:34 +0400

    Refactor API endpoint in server.js

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,10 +1,9 @@
 import { json } from '@sveltejs/kit';
-import { getMovies } from '$lib/api/api.js';
+import { getData } from '$lib/api/api.js';
 
 export async function POST({ request }) {
-  const { time , type } = await request.json()
+	const { time, type } = await request.json();
 
-  const moviesData = await getMovies(`/trending/${type}/${time}`);
-  return json({ status: 200, moviesData });
-
+	const moviesData = await getData(`/trending/${type}/${time}`);
+	return json({ status: 200, moviesData });
 }
