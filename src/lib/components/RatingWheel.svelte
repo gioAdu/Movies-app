e7f09bd757b575c7699fc53178e46857d1fe9336
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-08 11:50:28 +0400

    Add RatingWheel component

---
+++ src/lib/components/RatingWheel.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let movie;
+	const getRating = (rating) => {
+		return Math.round(rating * 10);
+	};
+
+	const getRatingClass = (rating) => {
+		const score = getRating(rating);
+
+		if (score > 70) {
+			return 'text-success';
+		} else if (score > 50) {
+			return 'text-warning';
+		} else {
+			return 'text-error';
+		}
+	};
+</script>
+
+<div
+	class="radial-progress bg-black
+    {getRatingClass(movie.vote_average)}"
+	style="--value:{getRating(movie.vote_average)}; --size: 3rem"
+	role="progressbar"
+>
+	{getRating(movie.vote_average)}
+</div>
