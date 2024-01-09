8a4515457df9940e8d494039e93cd065ce9b87ed
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-09 18:44:51 +0400

    Refactor load function in +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,6 @@
 import { getData } from '../lib/api/api.js';
 
-export async function load({ params }) {
+export async function load() {
 	const moviesData = await getData(`/trending/movie/day`);
 	const TVData = await getData(`/trending/tv/day`);
 
