0d46cd035782c7b411117355546afb5493ba6c66
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-09 18:44:44 +0400

    Add BurgerMenu component to Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,15 +3,13 @@
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
 	import DropDownButton from './DropDownButton.svelte';
-
+	import BurgerMenu from './BurgerMenu.svelte';
 	let isDark = JSON.parse($page.data.isDark);
 
 	onMount(() => {
 		const cookieIsDark = document.cookie.split('; ').find((row) => row.startsWith('isDark'));
 		isDark = cookieIsDark ? JSON.parse(cookieIsDark.split('=')[1]) : false;
 	});
-
-	let enableDropdownHover = true;
 </script>
 
 <nav class="bg-neutral">
@@ -19,11 +17,17 @@
 		<div class="flex gap-2">
 			<a href="/" class="text-4xl text-white">Home</a>
 
-			<DropDownButton title="Movies" items={['Popular', 'Top rated']} />
+			<div class=" hidden sm:block">
+				<DropDownButton title="Movies" items={['Popular', 'Top rated']} />
 
-			<DropDownButton title="TV Shows" items={['Popular', 'Top rated']} />
+				<DropDownButton title="TV Shows" items={['Popular', 'Top rated']} />
+			</div>
 		</div>
 
 		<ThemeController {isDark} />
+
+		<div class="sm:hidden">
+			<BurgerMenu />
+		</div>
 	</div>
 </nav>
