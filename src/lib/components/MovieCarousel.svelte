e9d76e0e5e78e046677ee41df89efb8ce762d72f
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-10 12:26:23 +0400

    Update MovieCarousel options and plugins

--- src/lib/components/MovieCarousel.svelte
+++ src/lib/components/MovieCarousel.svelte
@@ -8,8 +8,8 @@
 	export let showWheel = true;
 
 	let emblaApi;
-	let options = { loop: false, dragFree: true, containScroll: 'keepSnaps', slidesToScroll: 2 };
-	let plugins = [Autoplay({ delay: 5000, stopOnMouseEnter: true, stopOnInteraction: false })];
+	let options = { dragFree: true, slidesToScroll: 2};
+	let plugins = [Autoplay({ delay: 2000, stopOnMouseEnter: true, stopOnInteraction: false })];
 
 	const onInit = (event) => {
 		emblaApi = event.detail;
@@ -20,22 +20,22 @@
 	<div class="embla__container" transition:slide={{ duration: 400 }}>
 		{#each data as movie}
 			<div
-				class="embla__slide p-2 shrink-0 grow-0 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4 mx-auto"
+				class="embla__slide p-2 shrink-0 grow-0 basis-full sm:basis-1/2 md:basis-1/3 xl:basis-1/4 mx-auto"
 			>
 				<div class="card bg-base-100 shadow-md">
-					<div class="relative">
-						{#if showWheel}
-							<div class="left-16 absolute -bottom-6">
+					<div class="flex justify-center relative">
+						<div class='relative'>
+							{#if showWheel}
+							<div class="-left-2 absolute -bottom-5">
 								<RatingWheel {movie} />
 							</div>
 						{/if}
-						<figure>
 							<img
 								class="rounded-md"
 								src={`https://image.tmdb.org/t/p/w220_and_h330_face${movie.poster_path}`}
 								alt="Shoes"
 							/>
-						</figure>
+						</div>
 					</div>
 					<div class="card-body">
 						<h2 class="card-title line-clamp-1">{movie.title || movie.name}</h2>
