ebe03bdc7c61c41e10d589da1dac48c1cd71dc6c
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-11 15:24:10 +0400

    Refactor CastCarousel component to use checkProfileImage helper function

--- src/lib/components/features/CastCarousel.svelte
+++ src/lib/components/features/CastCarousel.svelte
@@ -1,21 +1,19 @@
 <script>
 	import emblaCarouselSvelte from 'embla-carousel-svelte';
 	import Autoplay from 'embla-carousel-autoplay';
+	import { checkProfileImage } from '$lib/helpers/checkProfileImage.js';
 	import { slide } from 'svelte/transition';
 
 	export let data;
 
 	let emblaApi;
-	let options = { loop: false, dragFree: true, containScroll: 'keepSnaps', slidesToScroll: 2 };
-	let plugins = [Autoplay({ delay: 5000, stopOnMouseEnter: true, stopOnInteraction: false })];
+	const options = { loop: false, dragFree: true, containScroll: 'keepSnaps', slidesToScroll: 2 };
+	const plugins = [Autoplay({ delay: 5000, stopOnMouseEnter: true, stopOnInteraction: false })];
+	const baseUrl = 'https://image.tmdb.org/t/p/w470_and_h470_face';
 
 	const onInit = (event) => {
 		emblaApi = event.detail;
 	};
-
-	const checkProfileImage = (cast) => {
-		return cast.profile_path ? `https://image.tmdb.org/t/p/w138_and_h175_face${cast.profile_path}` : '/default_icon.svg';
-	}
 </script>
 
 <div class="embla" use:emblaCarouselSvelte={{ options, plugins }} on:emblaInit={onInit}>
@@ -24,14 +22,10 @@
 			<div
 				class="embla__slide p-2 shrink-0 grow-0 basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/6"
 			>
-				<div class="card bg-base-100 shadow-md  h-full">
+				<div class="card bg-base-100 shadow-md h-full">
 					<div class="relative">
 						<figure>
-							<img
-								class="rounded-md"
-								src={checkProfileImage(cast)}
-								alt={cast.name}
-							/>
+							<img class="rounded-md" src={checkProfileImage(cast, baseUrl)} alt={cast.name} />
 						</figure>
 					</div>
 					<div class="card-body">
