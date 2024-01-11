e6c4af73a16ce65a4dcc5fa95fb6eeb2bf2a64f2
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2024-01-11 14:55:10 +0400

    Add pagination server route

---
+++ src/routes/api/pagination/+server.js
@@ -0,0 +1,14 @@
+import { json } from '@sveltejs/kit';
+import { getData } from '$lib/api/api.js';
+
+export async function POST({request} ) {
+	const pageData = await request.json();
+
+	const { showType, showcase } = pageData.params ;
+
+	const page = pageData.pageNumber;
+
+	const newData = await getData(`/${showType}/${showcase}?page=${page}`);
+
+	return json({ status: 200, newData });
+}
